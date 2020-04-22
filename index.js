25f5fbb8d1435f545611e3559c5696276310e5be
Max Ivanov <maximiv@gmail.com>
2020-04-22 01:24:23 +0100

    refactor: add error classes to module exports

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict'
 
 const { verifierFactory } = require('./src/verifier')
+const JwksFetchError = require('./src/errors/jwks-fetch-error')
+const JwtCognitoClaimValidationError = require('./src/errors/jwt-cognito-claim-validation-error')
+const JwtVerificationError = require('./src/errors/jwt-verification-error')
 
 module.exports = {
   verifierFactory,
+  errors: {
+    JwksFetchError,
+    JwtCognitoClaimValidationError,
+    JwtVerificationError,
+  },
 }
